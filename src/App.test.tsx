6de841e5883c31c268b8c0b312a8c95eee36dd03
maesterzak <abkdking1703@gmail.com>
2024-03-16 04:14:27 +0100

    add test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.setTimeout(10000);
+// Mocking fetch function
+global.fetch = jest.fn().mockResolvedValue({
+  ok: true,
+  json: () => Promise.resolve({ results: [], totalPages: 100 })
+}) as jest.Mock<Promise<Response>>;
+
+
+describe('App Component', () => {
+  test('renders loading text initially', async () => {
+    const { getByText } = render(<App />);
+    expect(getByText('Loading...')).toBeInTheDocument();
+    await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1));
+  });
+
+  
 });
